69073aa0f6837bfd4ddf6d5fdb714c3f06703bd7
Denis Volokh <dvolokh@corepartners.com>
2023-03-18 15:45:02 -0400

    Added login method

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -34,6 +34,32 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        //check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'login or password is incorrect'
+            ], 401);
+        }
+
+        $token = $user->createToken('petsapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
     public function logout(Request $request){
         auth()->user()->tokens()->delete();
 
